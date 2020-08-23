7de44dc842af78d41db0d038b26e8507cbcbb1f2
Michał Poręba <michalporeba@gmail.com>
2020-08-23 18:04:15 +0100

    window with a menu

--- src/main/java/com/michalporeba/golf/GameOfLife.java
+++ src/main/java/com/michalporeba/golf/GameOfLife.java
@@ -1,8 +1,11 @@
 package com.michalporeba.golf;
 
 import javafx.application.Application;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GameOfLife extends Application {
@@ -15,7 +18,13 @@ public class GameOfLife extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Game of Life");
 
-        StackPane root = new StackPane();
+        MenuBar menuBar = new MenuBar();
+        Menu menu1 = new Menu("Hello");
+        menuBar.getMenus().add(menu1);
+
+        VBox root = new VBox(menuBar);
+
+
         primaryStage.setScene(new Scene(root, 300, 300));
         primaryStage.show();
     }
